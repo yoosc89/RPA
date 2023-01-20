9909ed3f32f07044d83be2ca269ed5fcf2726e7f
yoosc89 <yoosc89@gmail.com>
2023-01-20 13:43:29 +0900

    .

--- 901_Telegram/001_hotdeal/info.py
+++ 901_Telegram/001_hotdeal/info.py
@@ -1,15 +1,13 @@
 def token():
     token = '5796656211:AAFBV4GPyKYLe0FOobwHiSL6u6QyZwrZhuU'
     return token
-    
+
 
 def mysql_account():
     account = {
-    'host' : 'tysct.kr',
-    'user' : 'webroot',
-    'password' : 'SC*750(synol',
-    'db' : 'telegram_bot',
+        'host': 'tysct.kr',
+        'user': 'webroot',
+        'password': 'SC*750(synol',
+        'db': 'telegram_bot',
     }
     return account
-
-
